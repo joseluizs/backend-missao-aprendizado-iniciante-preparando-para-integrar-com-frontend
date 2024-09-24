968e1c982fdefc99a070fc31331579b235dd8d58
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-24 20:33:48 +0000

    Atualização no `DB`

--- src/personagem/personagem.entity.js
+++ src/personagem/personagem.entity.js
@@ -1,7 +1,20 @@
 const Joi = require('joi');
 
 const personagem = Joi.object({
-  nome: Joi.string().min(3).max(30).required(),
+  nome: 
+    Joi.string()
+    .min(1)
+    .max(30)
+    .required(),
+  
+  imagem: Joi.string()
+    .uri()
+    .required(),
+
+  evoluiPara: Joi.string()
+    .min(1)
+    .max(30)
+    .optional()
 });
 
 module.exports = personagem
